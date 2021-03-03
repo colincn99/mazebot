b7e9a3dbf7bc731cbce774604e09c7f1da9c63ec
cchun9999 <cchun@caltech.edu>
2021-03-02 22:29:46 -0800

    added ability for python to change goal

---
+++ src/set_goal.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+#
+#   set_goal.py
+#
+#   Test setting the goal.
+#
+import rospy
+import numpy as np
+from geometry_msgs.msg import PoseStamped
+
+#
+#  Main Code
+#
+if __name__ == "__main__":
+	# Prepare the node.
+	rospy.init_node('set_goal')
+
+	# Create a publisher to send the joint values (joint_states).
+	pub = rospy.Publisher("/move_base_simple/goal", PoseStamped, queue_size=1)
+	goal = PoseStamped()
+
+	while True:
+
+		# Wait until connected.  You don't have to wait, but the first
+		# messages might go out before the connection and hence be lost.
+		rospy.sleep(0.25)
+
+		user_input = input("Enter x_position, y_position, orientation\n")
+		if user_input == "exit":
+			break
+		try:
+			x, y, theta = user_input.split(",")
+			x = float(x)
+			y = float(y)
+			theta = float(theta)
+		except:
+			print("incorrect format")
+			continue
+
+		goal.header.seq = 1.0
+		goal.header.stamp = rospy.Time.now()
+		goal.header.frame_id = "map"
+
+		goal.pose.position.x = x
+		goal.pose.position.y = y
+		goal.pose.position.z = 0.0
+
+		goal.pose.orientation.x = 0.0
+		goal.pose.orientation.y = 0.0
+		goal.pose.orientation.z = np.sin(theta/2)
+		goal.pose.orientation.w = np.cos(theta/2)
+
+		pub.publish(goal)
